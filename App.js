dcc7595157769f31f43ab0adf4fcbc626f4f9155
Anton Kobyshev <antonkobyshev@gmail.com>
2023-09-12 14:08:13 +0300

    done

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+
+import { LoginScreen } from "./Screens/LoginScreen/LoginScreen";
+import { RegistrationScreen } from "./Screens/RegistrationScreen/RegistrationScreen";
+import { PostsScreen } from "./Screens/PostsScreen/PostsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My first react native app</Text>
+      <RegistrationScreen />
+      {/* <LoginScreen />
+      <PostsScreen /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
